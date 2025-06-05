eb4ede75dced4634ce5fee69115742c25e9089b9
FELIPE BECKFORD SISIS <49255481@ort>
2025-06-05 15:26:41 -0300

    Bastantes Avances 5/6. Model Nivel casi terminado y controller casi terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,110 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+
         return View();
+    }
+    public IActionResult Jugar()
+    {
+        Nivel niveles = new Nivel();
+        niveles.InicializarNivel1();
+        HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+        ViewBag.pista = niveles.pistas[0];
+        return View("Nivel1");
+    }
+
+
+    public IActionResult Nivel1(string rta)
+    {
+        Nivel niveles = Objeto.StringToObject<Nivel>(HttpContext.Session.GetString("niveles")) ;
+        if(niveles.comprobarRespuesta(rta, niveles.numNivel))
+        {
+            HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+            return View("Nivel2");
+        }else return View();
+    }
+
+    public IActionResult Nivel2(string rta)
+    {
+        Nivel niveles = Objeto.StringToObject<Nivel>(HttpsContext.Session.GetString("niveles"));
+        niveles.InicializarNivel2();
+        if(!niveles.verificarAvance)
+        {
+            return View("Jugar");
+        }
+        HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+        ViewBag.pista = niveles.pistas[1];
+
+        if(niveles.comprobarRespuesta(rta, niveles.numNivel))
+        {
+            HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+            return View("Nivel3");
+        }else return View();
+    }
+
+     public IActionResult Nivel3(string rta)
+    {
+        Nivel niveles = Objeto.StringToObject<Nivel>(HttpsContext.Session.GetString("niveles"));
+        niveles.InicializarNivel3();
+        if(!niveles.verificarAvance)
+        {
+            return View("Jugar");
+        }
+        HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+        ViewBag.pista = niveles.pistas[2];
+
+        if(niveles.comprobarRespuesta(rta, niveles.numNivel))
+        {
+            HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+            return View("Nivel4");
+        }else return View();
+    }
+
+         public IActionResult Nivel4(string[] rta)
+    {
+        Nivel niveles = Objeto.StringToObject<Nivel>(HttpsContext.Session.GetString("niveles"));
+        niveles.InicializarNivel4();
+        if(!niveles.verificarAvance)
+        {
+            return View("Jugar");
+        }
+        HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+        ViewBag.pista = niveles.pistas[3];
+
+
+        string resp = "";
+        for (int i = 0; i < rta.Length; i++)
+        {
+        resp += rta[i];
+
+        if (i < rta.Length - 1)
+        {
+        resp += ",";
+        }
+        }
+        
+        if(niveles.comprobarRespuesta(resp, niveles.numNivel))
+        {
+            HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+            return View("Nivel5");
+        }else return View();
+    }
+
+     public IActionResult Nivel5(string rta)
+    {
+        Nivel niveles = Objeto.StringToObject<Nivel>(HttpsContext.Session.GetString("niveles"));
+        niveles.InicializarNivel5();
+        if(!niveles.verificarAvance)
+        {
+            return View("Jugar");
+        }
+        HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+        ViewBag.pista = niveles.pistas[4];
+
+        if(niveles.comprobarRespuesta(rta, niveles.numNivel))
+        {
+            HttpContext.Session.SetString("niveles", Objeto.ObjectToString(niveles));
+            return View("Ganar");
+        }else return View();
     }
 }
--- Models/Nivel.cs
+++ Models/Nivel.cs
@@ -10,13 +10,70 @@ public class Nivel
     [JsonProperty]
     public string[] pistas {get; private set;}
 
+    [JsonProperty]
+    public int nivelesCompletados {get; private set;}
+    public Nivel()
+    {
+        this.respuestas = new string[5];
+        this.pistas = new string[5];
+    }
 
+    public bool verificarAvance()
+    {
+        bool aux = true;
+
+        if(nivelesCompletados > numNivel)
+        {
+            aux = false;
+        }
+        return aux;
+    }
     public void InicializarNivel1()
     {
+        respuestas[0] = "erbilorgamla";
+        numNivel = 1;
+        pistas[0] = "Piensa al reves de como lo sueles hacer...";
         
+    }
+
+    public void InicializarNivel2()
+    {
+        respuestas[1] = "Ceriana";
+        numNivel = 2;
+        pistas[1] = "4 palabras: Barba, Lentes, Mate y HTML";
+    }
+
+       public void InicializarNivel3()
+    {
+        respuestas[2] = null;
+        numNivel = 3;
+        pistas[2] = "Gracias a mi ves todo lo que ves...";
+    }
+
+       public void InicializarNivel4()
+    {
+        respuestas[3] = "Fotos_Privadas.png,Contraseña_Gmail.txt,bestChatGPTPrompts.txt,troyano.bat,chatPrivado.txt";
+        numNivel = 4;
+        pistas[3] = "maximo 5 solo capos only caps";
+    }
+
+       public void InicializarNivel5()
+    {
+        respuestas[4] = "531426";
+        numNivel = 5;
+        pistas[4] = "Como dijo Leo: MVC = Model, View, Controller.";
     }
     //debe haber varios inicializarNivel y estos seran usados en cada action...
 
+    public bool comprobarRespuesta(string respuesta, int nivel)
+    {
+        bool aux = false;
+        if(respuesta == respuesta[nivel-1])
+        {
+            aux = true;
+        }
+        return aux;
+    }
 
     
 }
